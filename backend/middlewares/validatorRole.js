d64c90ee33297abeb3e0ce69bfddcdce6195c86f
Gabriel <gszcode@gmail.com>
2022-10-16 04:34:03 -0300

    validacion de rol

---
+++ backend/middlewares/validatorRole.js
@@ -0,0 +1,14 @@
+import User from '../models/userModel.js'
+
+export const validatorRole = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.uid)
+
+    if (user.role !== 'admin') throw new Error('Not authorized')
+
+    next()
+    console.log(user)
+  } catch (error) {
+    return res.status(401).json({ error: error.message })
+  }
+}
