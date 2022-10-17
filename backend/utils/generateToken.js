8b901e65c32633b1f4066d31ac502755c2956afa
Gabriel <gszcode@gmail.com>
2022-10-17 20:41:21 -0300

    configuraciones e instalacion de dependencias del frontend

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,7 +7,7 @@ export const generateToken = (uid, res) => {
     expiresIn
   })
 
-  // Options cookit
+  // Options cookie
   const options = {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
