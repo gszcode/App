98024332459a433184bbe6c49ef5474044aa07bb
Gabriel <gszcode@gmail.com>
2022-10-19 22:43:12 -0300

    corrección de nombre de clases

--- frontend/src/Pages/Landing.jsx
+++ frontend/src/Pages/Landing.jsx
@@ -5,9 +5,9 @@ export default function Landing() {
   return (
     <section className="landing">
       <div className="landing__welcome">
-        <h1 className="landing__welcome--title">Welcome to Ecommerce</h1>
-        <h3 className="landing__welcome--subtitle">FIND AMAZING PRODUCTS</h3>
-        <a href="#features" className="landing__welcome--scroll">
+        <h1 className="landing__welcome__title">Welcome to Ecommerce</h1>
+        <h3 className="landing__welcome__subtitle">FIND AMAZING PRODUCTS</h3>
+        <a href="#features" className="landing__welcome__scroll">
           Scroll <BsMouse />
         </a>
       </div>
