3af3c5e60ae59d0cec0209d71e8bd3598b20d431
Gabriel <gszcode@gmail.com>
2022-10-20 04:38:19 -0300

    clases nuevas para modo responsive

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,33 @@
 import { Link } from 'react-router-dom'
-import { FaSistrix, FaShoppingCart, FaUser } from 'react-icons/fa'
+import {
+  FaSistrix,
+  FaShoppingCart,
+  FaUser,
+  FaBars,
+  FaTimes
+} from 'react-icons/fa'
 import logo from '../../images/logo.png'
 import './Navbar.scss'
+import { useState } from 'react'
 
 export const Navbar = () => {
+  const [barsState, setBarsState] = useState(false)
+
+  function handleClick() {
+    setBarsState((prevState) => !prevState)
+  }
+
   return (
-    <nav className="navbar navbar__mobile">
+    <nav className="navbar">
       <div className="navbar__image navbar__image--mobile">
         <img src={logo} alt="Logo" className="navbar__image__logo" />
       </div>
-      <div className="navbar__menu">
+      <div className="navbar__bars--mobile">
+        <button onClick={handleClick} className="navbar__bars__button--mobile">
+          {barsState ? <FaTimes /> : <FaBars />}
+        </button>
+      </div>
+      <div className={barsState ? 'navbar__menu' : 'navbar__menu--mobile'}>
         <Link className="navbar__menu__link" to="/">
           Home
         </Link>
@@ -23,7 +41,7 @@ export const Navbar = () => {
           About
         </Link>
       </div>
-      <div className="navbar__icons">
+      <div className={barsState ? 'navbar__icons' : 'navbar__icons--mobile'}>
         <FaSistrix className="navbar__icons__item" />
         <FaShoppingCart className="navbar__icons__item" />
         <FaUser className="navbar__icons__item" />
