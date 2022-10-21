0bcc70b2f85c55af9f2ae6aedda8ed06d97bbc1e
Gabriel <gszcode@gmail.com>
2022-10-20 22:41:55 -0300

    componente featured agregado a la app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import { Navbar } from './components/Navbar/Navbar'
-import Landing from './Pages/Landing'
+import Landing from './Pages/Landing/Landing'
 import Footer from './components/Footer/Footer'
 import { Routes, Route } from 'react-router-dom'
+import { FeaturedProducts } from './Pages/FeaturedProducts/FeaturedProducts'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
       </Routes>
+      <FeaturedProducts />
       <Footer />
     </>
   )
