0715829707831300d995a4eead1b411ae557ee27
Gabriel <gszcode@gmail.com>
2022-10-21 00:35:18 -0300

    links a los iconos

--- frontend/src/Pages/Landing/Landing.jsx
+++ frontend/src/Pages/Landing/Landing.jsx
@@ -1,16 +1,20 @@
 import './Landing.scss'
 import { BsMouse } from 'react-icons/bs'
+import { FeaturedProducts } from '../FeaturedProducts/FeaturedProducts'
 
 export default function Landing() {
   return (
-    <section className="landing">
-      <div className="landing__welcome">
-        <h1 className="landing__welcome__title">Welcome to Ecommerce</h1>
-        <h3 className="landing__welcome__subtitle">FIND AMAZING PRODUCTS</h3>
-        <a href="#featured" className="landing__welcome__scroll">
-          Scroll <BsMouse />
-        </a>
-      </div>
-    </section>
+    <>
+      <section className="landing">
+        <div className="landing__welcome">
+          <h1 className="landing__welcome__title">Welcome to Ecommerce</h1>
+          <h3 className="landing__welcome__subtitle">FIND AMAZING PRODUCTS</h3>
+          <a href="#featured" className="landing__welcome__scroll">
+            Scroll <BsMouse />
+          </a>
+        </div>
+      </section>
+      <FeaturedProducts />
+    </>
   )
 }
