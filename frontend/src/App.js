6079be83fcb9bdf9a5b7ea5318a2acbd8844b9e6
Gabriel <gszcode@gmail.com>
2022-10-21 04:40:48 -0300

    componente about agregado a app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
-import { Navbar } from './components/Navbar/Navbar'
-import Landing from './Pages/Landing/Landing'
-import Footer from './components/Footer/Footer'
 import { Routes, Route } from 'react-router-dom'
+import { Navbar } from './components/Navbar/Navbar'
+import { Landing } from './Pages/Landing/Landing'
 import { Search } from './components/Search/Search'
 import { Contact } from './Pages/Contact/Contact'
+import { About } from './Pages/About/About'
+import { Footer } from './components/Footer/Footer'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
         <Route path="/search" element={<Search />} />
       </Routes>
       <Footer />
