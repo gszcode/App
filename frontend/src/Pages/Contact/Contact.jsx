e9c3222b9127d1525ad535bd19e9850ce033d13e
Gabriel <gszcode@gmail.com>
2022-10-21 03:24:03 -0300

    componente de contacto creado

---
+++ frontend/src/Pages/Contact/Contact.jsx
@@ -0,0 +1,47 @@
+import { FaGithub, FaLinkedin, FaGlobe } from 'react-icons/fa'
+import './Contact.scss'
+
+export const Contact = () => {
+  return (
+    <div className="contact">
+      <a className="contact__email" href="mailto:gszcode@gmail.com">
+        CONTACT: GSZCODE@GMAIL.COM
+      </a>
+      <div className="contact__links">
+        <span className="contact__links__item">
+          <FaGlobe className="social" />
+          <a
+            className="social__name"
+            href="https://gszcode.github.io/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            PORTFOLIO
+          </a>
+        </span>
+        <span className="contact__links__item">
+          <FaLinkedin className="social" />
+          <a
+            className="social__name"
+            href="https://www.linkedin.com/in/gabriel-sanchez-0591a723a/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            LINKEDIN
+          </a>
+        </span>
+        <span className="contact__links__item">
+          <FaGithub className="social" />
+          <a
+            className="social__name"
+            href="https://github.com/gszcode"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GITHUB
+          </a>
+        </span>
+      </div>
+    </div>
+  )
+}
