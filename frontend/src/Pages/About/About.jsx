9005db68d120ffcdcb828751c3070eab97147191
Gabriel <gszcode@gmail.com>
2022-10-21 04:41:54 -0300

    componente about completo con estilos y responsive

---
+++ frontend/src/Pages/About/About.jsx
@@ -0,0 +1,23 @@
+import photo from '../../images/me.jpg'
+import './About.scss'
+
+export const About = () => {
+  return (
+    <section className="about">
+      <div className="about__left"></div>
+      <div className="about__profile">
+        <h2>About Us</h2>
+        <div className="me">
+          <img className="me__photo" src={photo} alt="Profile" />
+          <h4 className="me__name">Gabriel Sanchez</h4>
+          <p className="me__parrafo">
+            this is a sample website made by @gszcode. Only with the purpose to
+            practice MERN STACK.
+          </p>
+        </div>
+        <span className="about__span">Contact Me</span>
+      </div>
+      <div className="about__right"></div>
+    </section>
+  )
+}
