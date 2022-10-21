914b6922d38b21b0173e5c0a9d3201aab800d403
Gabriel <gszcode@gmail.com>
2022-10-21 03:23:45 -0300

    componente de contacto agregado al app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Landing from './Pages/Landing/Landing'
 import Footer from './components/Footer/Footer'
 import { Routes, Route } from 'react-router-dom'
 import { Search } from './components/Search/Search'
+import { Contact } from './Pages/Contact/Contact'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/search" element={<Search />} />
       </Routes>
       <Footer />
