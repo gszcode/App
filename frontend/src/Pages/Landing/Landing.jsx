49df1a515bdafe880b7c2a7f052ee0045292e085
Gabriel <gszcode@gmail.com>
2022-10-20 22:39:57 -0300

    link a featured products

---
+++ frontend/src/Pages/Landing/Landing.jsx
@@ -0,0 +1,16 @@
+import './Landing.scss'
+import { BsMouse } from 'react-icons/bs'
+
+export default function Landing() {
+  return (
+    <section className="landing">
+      <div className="landing__welcome">
+        <h1 className="landing__welcome__title">Welcome to Ecommerce</h1>
+        <h3 className="landing__welcome__subtitle">FIND AMAZING PRODUCTS</h3>
+        <a href="#featured" className="landing__welcome__scroll">
+          Scroll <BsMouse />
+        </a>
+      </div>
+    </section>
+  )
+}
