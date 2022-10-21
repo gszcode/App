14cc86380dbe7a4e33fabbe2f7658d7d22a098c6
Gabriel <gszcode@gmail.com>
2022-10-21 00:34:09 -0300

    productos destacados movido a landing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import { Navbar } from './components/Navbar/Navbar'
 import Landing from './Pages/Landing/Landing'
 import Footer from './components/Footer/Footer'
 import { Routes, Route } from 'react-router-dom'
-import { FeaturedProducts } from './Pages/FeaturedProducts/FeaturedProducts'
+import { Search } from './components/Search/Search'
 
 function App() {
   return (
@@ -10,8 +10,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/search" element={<Search />} />
       </Routes>
-      <FeaturedProducts />
       <Footer />
     </>
   )
