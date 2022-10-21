830b2a30e0978d90b4e1aa218892e3aa6464179c
Gabriel <gszcode@gmail.com>
2022-10-20 23:45:10 -0300

    Productos que se muestra en la landing como destacados

--- frontend/src/Pages/FeaturedProducts/Product.jsx
+++ frontend/src/Pages/FeaturedProducts/Product.jsx
@@ -1,14 +1,17 @@
+import { Link } from 'react-router-dom'
 import './FeaturedProducts.scss'
 
-export const Product = ({ img, name, rating, reviews, price }) => {
+export const Product = ({ id, img, name, rating, reviews, price }) => {
   return (
-    <article className="featured__product">
-      <img className="featured__product__img" src={img} alt={name} />
-      <h3 className="featured__product__title">{name}</h3>
-      <p className="featured__product__rating">
-        {rating} ({reviews} reviews)
-      </p>
-      <span className="featured__product__price">{price}</span>
-    </article>
+    <Link to={`/products/${id}`}>
+      <article className="featured__product">
+        <img className="featured__product__img" src={img} alt={name} />
+        <h3 className="featured__product__title">{name}</h3>
+        <p className="featured__product__rating">
+          {rating} ({reviews} reviews)
+        </p>
+        <span className="featured__product__price">{price}</span>
+      </article>
+    </Link>
   )
 }
