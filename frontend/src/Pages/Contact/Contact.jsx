4684c92600926dd446e9bc1442622451118ccf74
Gabriel <gszcode@gmail.com>
2022-10-22 03:41:36 -0300

    nuevo contenedor para ponerle un background gradient

--- frontend/src/Pages/Contact/Contact.jsx
+++ frontend/src/Pages/Contact/Contact.jsx
@@ -3,44 +3,46 @@ import './Contact.scss'
 
 export const Contact = () => {
   return (
-    <section className="contact">
-      <a className="contact__email" href="mailto:gszcode@gmail.com">
-        CONTACT: GSZCODE@GMAIL.COM
-      </a>
-      <div className="contact__links">
-        <span className="contact__links__item">
-          <FaGlobe className="social" />
-          <a
-            className="social__name"
-            href="https://gszcode.github.io/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            PORTFOLIO
-          </a>
-        </span>
-        <span className="contact__links__item">
-          <FaLinkedin className="social" />
-          <a
-            className="social__name"
-            href="https://www.linkedin.com/in/gabriel-sanchez-0591a723a/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            LINKEDIN
-          </a>
-        </span>
-        <span className="contact__links__item">
-          <FaGithub className="social" />
-          <a
-            className="social__name"
-            href="https://github.com/gszcode"
-            target="_blank"
-            rel="noreferrer"
-          >
-            GITHUB
-          </a>
-        </span>
+    <section className="container">
+      <div className="contact">
+        <a className="contact__email" href="mailto:gszcode@gmail.com">
+          CONTACT: GSZCODE@GMAIL.COM
+        </a>
+        <div className="contact__links">
+          <span className="contact__links__item">
+            <FaGlobe className="social" />
+            <a
+              className="social__name"
+              href="https://gszcode.github.io/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              PORTFOLIO
+            </a>
+          </span>
+          <span className="contact__links__item">
+            <FaLinkedin className="social" />
+            <a
+              className="social__name"
+              href="https://www.linkedin.com/in/gabriel-sanchez-0591a723a/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              LINKEDIN
+            </a>
+          </span>
+          <span className="contact__links__item">
+            <FaGithub className="social" />
+            <a
+              className="social__name"
+              href="https://github.com/gszcode"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GITHUB
+            </a>
+          </span>
+        </div>
       </div>
     </section>
   )
