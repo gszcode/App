567326a9ba595663ae52cc371283d1d36a9c4ec7
Gabriel <gszcode@gmail.com>
2022-10-26 23:41:12 -0300

    validaciones

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -6,39 +6,68 @@ import {
   AiFillEyeInvisible
 } from 'react-icons/ai'
 import { Link } from 'react-router-dom'
+import { validateLogin } from '../../utils/errorsForms'
 import './Login.scss'
 
 export const Login = () => {
+  const user = { email: '', password: '' }
   const [seePassword, setSeePassword] = useState(false)
+  const [form, setForm] = useState(user)
+  const [errors, setErrors] = useState(user)
 
   function handleClick() {
     setSeePassword((prevValue) => !prevValue)
   }
 
+  function handleChange(e) {
+    const { name, value } = e.target
+
+    setForm({ ...form, [name]: value })
+    setErrors(validateLogin({ ...form, [name]: value }))
+  }
+
+  console.log({ form })
+  console.log({ errors })
+
+  function handleSubmit(e) {
+    e.preventDefault()
+  }
+
   return (
-    <div className="login">
+    <form onSubmit={handleSubmit} className="login">
       <div className="login__input">
         <span className="login__input__icon">
           <AiOutlineMail />
         </span>
-        <input type="email" placeholder="Email" />
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          value={form.email}
+          onChange={handleChange}
+        />
+        <span style={{ 'font-size': '15px' }}>{errors.email}</span>
       </div>
       <div className="login__input">
         <span className="login__input__icon">
           <AiOutlineUnlock />
         </span>
         <input
+          name="password"
           type={seePassword ? 'text' : 'password'}
           placeholder="Password"
+          value={form.password}
+          onChange={handleChange}
         />
         <span onClick={handleClick} className="eye">
           {seePassword ? <AiFillEyeInvisible /> : <AiFillEye />}
         </span>
+        <span style={{ 'font-size': '15px' }}>{errors.password}</span>
       </div>
       <Link to="/password/forgot" className="login__forget">
         Forget Password?
       </Link>
       <button className="login__btn">Login</button>
-    </div>
+    </form>
   )
 }
