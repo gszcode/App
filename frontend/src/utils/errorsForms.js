19edb16507127ab95b3d6aa6230fc6ee7abf7fac
Gabriel <gszcode@gmail.com>
2022-10-27 01:16:19 -0300

    funcion validadora de errores

--- frontend/src/utils/errorsForms.js
+++ frontend/src/utils/errorsForms.js
@@ -1,3 +1,20 @@
+export const validateRegister = (form) => {
+  let errors = {}
+
+  !form.user ? (errors.user = 'Required field.') : (errors.user = '')
+
+  !form.email ? (errors.email = 'Required field.') : (errors.email = '')
+
+  !form.password
+    ? (errors.password = 'Required field.')
+    : (errors.password = '')
+
+  !form.repassword
+    ? (errors.repassword = 'Required field.')
+    : (errors.repassword = '')
+
+  return errors
+}
 export const validateLogin = (form) => {
   let errors = {}
 
