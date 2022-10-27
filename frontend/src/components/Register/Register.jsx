1e992fc288a7a592a3728030af64201ff3bbf9da
Gabriel <gszcode@gmail.com>
2022-10-27 01:15:41 -0300

    validaciones y errores mostrados

--- frontend/src/components/Register/Register.jsx
+++ frontend/src/components/Register/Register.jsx
@@ -6,42 +6,127 @@ import {
   AiFillEye,
   AiFillEyeInvisible
 } from 'react-icons/ai'
+import { validateRegister } from '../../utils/errorsForms'
 import './Register.scss'
 
 export const Register = () => {
+  const user = { user: '', email: '', password: '', repassword: '' }
   const [seePassword, setSeePassword] = useState(false)
+  const [seeRePassword, setSeeRePassword] = useState(false)
+  const [errors, setErrors] = useState(user)
+  const [form, setForm] = useState(user)
 
-  function handleClick() {
+  function handleClickPassword(e) {
     setSeePassword((prevValue) => !prevValue)
   }
 
+  function handleClickRePassword(e) {
+    setSeeRePassword((prevValue) => !prevValue)
+  }
+
+  function handleChange(e) {
+    const { name, value } = e.target
+
+    setForm({ ...form, [name]: value })
+    setErrors(validateRegister({ ...form, [name]: value }))
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    const { name, value } = e.target
+
+    setErrors(validateRegister({ ...form, [name]: value }))
+  }
+
   return (
-    <div className="register">
-      <div className="register__input">
+    <form onSubmit={handleSubmit} className="register">
+      <div
+        className={
+          errors.user
+            ? 'register__input register__input--error'
+            : 'register__input'
+        }
+      >
         <span className="register__input__icon">
           <AiOutlineUser />
         </span>
-        <input type="text" placeholder="User" />
+        <input
+          name="user"
+          onChange={handleChange}
+          type="text"
+          placeholder="User"
+        />
+        <span className="register__error">{errors.user}</span>
       </div>
-      <div className="register__input">
+      <div
+        className={
+          errors.email
+            ? 'register__input register__input--error'
+            : 'register__input'
+        }
+      >
         <span className="register__input__icon">
           <AiOutlineMail />
         </span>
-        <input type="email" placeholder="Email" />
+        <input
+          name="email"
+          onChange={handleChange}
+          type="email"
+          placeholder="Email"
+        />
+        <span className="register__error">{errors.email}</span>
       </div>
-      <div className="register__input">
+      <div
+        className={
+          errors.password
+            ? 'register__input register__input--error'
+            : 'register__input'
+        }
+      >
         <span className="register__input__icon">
           <AiOutlineUnlock />
         </span>
         <input
+          name="password"
+          onChange={handleChange}
           type={seePassword ? 'text' : 'password'}
           placeholder="Password"
         />
-        <span onClick={handleClick} className="eye">
+        <span onClick={handleClickPassword} name="aye_pass" className="eye">
           {seePassword ? <AiFillEyeInvisible /> : <AiFillEye />}
         </span>
+        <span className="register__error">{errors.password}</span>
       </div>
-      <button className="register__btn">Register</button>
-    </div>
+      <div
+        className={
+          errors.password
+            ? 'register__input register__input--error'
+            : 'register__input'
+        }
+      >
+        <span className="register__input__icon">
+          <AiOutlineUnlock />
+        </span>
+        <input
+          name="repassword"
+          onChange={handleChange}
+          type={seeRePassword ? 'text' : 'password'}
+          placeholder="Repeat Password"
+        />
+        <span onClick={handleClickRePassword} className="eye">
+          {seeRePassword ? <AiFillEyeInvisible /> : <AiFillEye />}
+        </span>
+        <span className="register__error">{errors.repassword}</span>
+      </div>
+      <button
+        className={
+          !form.user || !form.email || !form.password || !form.repassword
+            ? 'register__btn register__btn--disabled'
+            : 'register__btn'
+        }
+      >
+        Register
+      </button>
+    </form>
   )
 }
