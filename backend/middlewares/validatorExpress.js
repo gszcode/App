1730d73a6048159a2ffa7e9ebc07b45788272d30
Gabriel <gszcode@gmail.com>
2022-10-28 19:10:48 -0300

    ajustando validadores de registro y login

--- backend/middlewares/validatorExpress.js
+++ backend/middlewares/validatorExpress.js
@@ -1,11 +1,10 @@
-import { body, validationResult } from 'express-validator'
+const { body, validationResult } = require('express-validator')
 
-// Errors
-export const resultValidator = (req, res, next) => {
+const resultValidator = (req, res, next) => {
   const errors = validationResult(req)
 
   if (!errors.isEmpty()) {
-    return res.json({ errors: errors.errors[0].msg })
+    return res.status(400).json({ message: errors.array() })
   }
 
   next()
