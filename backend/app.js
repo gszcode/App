86386ecdf992f164baa1d7706d06f9b8973b50f9
Gabriel <gszcode@gmail.com>
2022-10-28 19:09:20 -0300

    configuracion de cors

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,11 @@ import cookieParser from 'cookie-parser'
 import express from 'express'
 import morgan from 'morgan'
 import userRoutes from './routes/userRoute.js'
+import cors from 'cors'
 const app = express()
 
 // Middlewares
+app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(cookieParser())
