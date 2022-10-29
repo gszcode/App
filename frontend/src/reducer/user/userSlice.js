d2f25f1d91c0ff6635118191157310dbac029a1a
Gabriel <gszcode@gmail.com>
2022-10-29 04:33:44 -0300

    llamado register y login y guardado en store global (redux)

--- frontend/src/reducer/user/userSlice.js
+++ frontend/src/reducer/user/userSlice.js
@@ -1,18 +1,65 @@
 import { createSlice } from '@reduxjs/toolkit'
+import axios from 'axios'
+import Swal from 'sweetalert2'
 
 const initialState = {
-  allUsers: []
+  user: []
+  // errors: []
 }
 
 export const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    getAllUsers: (state, action) => {
-      state.allUsers = action.payload
+    getUser: (state, action) => {
+      state.user = action.payload
     }
+    // getErrors: (state, action) => {
+    //   state.errors = action.payload
+    // }
   }
 })
 
-export const { getAllUsers } = userSlice.actions
+export const { getUser, getErrors } = userSlice.actions
 export default userSlice.reducer
+
+// Llamadas a la api
+export const registerAndLoginUser = (url, form, loginOrRegister) => {
+  return (dispatch) => {
+    axios
+      .post(url, form)
+      .then((data) => {
+        dispatch(getUser(data.data))
+        Swal.fire({
+          title: 'Success!',
+          text:
+            loginOrRegister === 'login'
+              ? 'Successful Login'
+              : 'Successful Registration',
+          icon: 'success',
+          confirmButtonText: 'Close'
+        })
+      })
+      .catch((err) => {
+        const onlyError = err.response.data.message
+        if (onlyError) {
+          Swal.fire({
+            title: 'Error!',
+            text: onlyError,
+            icon: 'error',
+            confirmButtonText: 'Cool'
+          })
+        }
+
+        const errors = err.response.data.message
+        errors.forEach((e) => {
+          Swal.fire({
+            title: 'Error!',
+            text: e.msg,
+            icon: 'error',
+            confirmButtonText: 'Close'
+          })
+        })
+      })
+  }
+}
