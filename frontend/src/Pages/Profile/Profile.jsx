8b60bb887542f347bb5a338a35a1e769d0f04569
Gabriel <gszcode@gmail.com>
2022-11-02 07:37:51 -0300

    mostrando los datos del usuario

--- frontend/src/Pages/Profile/Profile.jsx
+++ frontend/src/Pages/Profile/Profile.jsx
@@ -1,16 +1,63 @@
 import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import { getProfileUser } from '../../reducer/user/userSlice'
+import { clearState, getProfileUser } from '../../reducer/user/userSlice'
+import { Loading } from '../../components/Loading/Loading'
 import './Profile.scss'
 
 export function Profile() {
   const dispatch = useDispatch()
+  const navigate = useNavigate()
   const { userData } = useSelector((state) => state.users)
-  console.log(userData)
 
   useEffect(() => {
-    dispatch(getProfileUser())
+    dispatch(getProfileUser('http://localhost:3001/api/v1/auth/profile'))
   }, [dispatch])
 
-  return <div className="profile">{/* <h1>{user.user.name}</h1> */}</div>
+  function handleClick() {
+    localStorage.clear()
+    dispatch(clearState())
+    navigate('/user-access')
+  }
+
+  return (
+    <>
+      {userData.success ? (
+        <section className="profile">
+          <div className="profile__container">
+            <div className="profile__photo">
+              <div className="profile__photo__img">
+                <img src={userData.user.avatar} alt="Avatar" />
+              </div>
+              <button className="profile__photo__button">Edit Profile</button>
+              {/* Cloudinary */}
+            </div>
+            <div className="profile__data">
+              <div className="profile__data__content">
+                <h3 className="item">Full Name</h3>
+                <p className="item">{userData.user.name.toUpperCase()}</p>
+              </div>
+              <div className="profile__data__content">
+                <h3 className="item">Email</h3>
+                <p className="item">{userData.user.email.toUpperCase()}</p>
+              </div>
+              <div className="profile__data__content">
+                <h3 className="item">Joined On</h3>
+                <p className="item">
+                  {userData.user.date.toUpperCase().slice(0, 10)}
+                </p>
+              </div>
+
+              <button className="profile__data__btn">My Orders</button>
+              <button onClick={handleClick} className="profile__data__btn">
+                Logout
+              </button>
+            </div>
+          </div>
+        </section>
+      ) : (
+        <Loading />
+      )}
+    </>
+  )
 }
