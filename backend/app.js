442bace87fe4bf293273b9aa7b9e0dfb53438206
Gabriel <gszcode@gmail.com>
2022-11-02 07:34:21 -0300

    cambio de posiciones de middlewares

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,15 @@
-import cookieParser from 'cookie-parser'
 import express from 'express'
 import morgan from 'morgan'
 import userRoutes from './routes/userRoute.js'
 import cors from 'cors'
+import cookieParser from 'cookie-parser'
 const app = express()
 
 // Middlewares
-app.use(cors())
-app.use(express.json())
 app.use(morgan('dev'))
+app.use(express.json())
 app.use(cookieParser())
+app.use(cors())
 
 // Routes
 app.use('/api/v1/auth', userRoutes)
