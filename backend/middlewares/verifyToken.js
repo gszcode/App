1a33c83378c598de3ff3fd19de749dfa040d1ec1
Gabriel <gszcode@gmail.com>
2022-11-02 07:41:06 -0300

    mensaje de error sobre token

--- backend/middlewares/verifyToken.js
+++ backend/middlewares/verifyToken.js
@@ -1,16 +1,20 @@
 import jwt from 'jsonwebtoken'
+import { tokenVerificationErrors } from '../utils/generateToken.js'
 
 export const verifyToken = (req, res, next) => {
-  const { token } = req.cookies
+  // const { token } = req.cookies
+  const token = req.headers.authorization.split(' ')[1]
 
   try {
-    if (!token) throw new Error('Token not exist')
+    if (!token) throw new Error('Please Login to access this resource')
 
     const { uid } = jwt.verify(token, process.env.JWT_SECRET)
     req.uid = uid
 
     next()
   } catch (error) {
-    return res.status(404).json({ error: error.message })
+    return res
+      .status(404)
+      .json({ error: tokenVerificationErrors[error.message] })
   }
 }
