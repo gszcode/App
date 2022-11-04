3108d5f5e67421a98b6953a5dd7c513b415a603a
Gabriel <gszcode@gmail.com>
2022-11-03 23:00:35 -0300

    config puerto

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import './database.js'
 import app from './app.js'
 
 // Initialization
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT} 🔥`)
 })
