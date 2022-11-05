775903c9776ba9c4f9840e6a04fc761d338c6c30
Gabriel <gszcode@gmail.com>
2022-11-05 03:51:31 -0300

    configuracion de cors

--- backend/app.js
+++ backend/app.js
@@ -6,10 +6,28 @@ import cookieParser from 'cookie-parser'
 const app = express()
 
 // Middlewares
+const whiteList = [
+  process.env.ORIGIN1,
+  'http://localhost:3000',
+  'http://127.0.0.1:3000'
+]
+express.defaultURL = process.env.ORIGIN1 || 'http://localhost:3000'
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || whiteList.includes(origin)) {
+        return callback(null, origin)
+      }
+      return callback('Error de CORS origin: ' + origin + ' No autorizado!')
+    },
+    credentials: true
+  })
+)
+
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors('*'))
 
 // Routes
 app.use('/api/v1/auth', userRoutes)
